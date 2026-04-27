6bc06d4e43e52ecdf4f9ae65fc014db3c13419c0
ItsKartavyaJ <kartavyaj70@gmail.com>
2026-04-27 23:26:52 +0530

    fix: restore parsedmarc influx writer sidecar

--- parsedmarc-stack/parsedmarc/entrypoint.sh
+++ parsedmarc-stack/parsedmarc/entrypoint.sh
@@ -2,8 +2,9 @@
 # Generate parsedmarc.ini from environment variables, then run parsedmarc
 cat > /tmp/parsedmarc.ini << EOF
 [general]
-save_aggregate = False
+save_aggregate = True
 save_forensic = False
+output = /data
 
 [imap]
 host = ${PARSEDMARC_IMAP_HOST}
@@ -18,12 +19,6 @@ delete = False
 batch_size = ${PARSEDMARC_MAILBOX_BATCH_SIZE:-10}
 reports_folder = ${PARSEDMARC_MAILBOX_REPORTS_FOLDER:-INBOX}
 archive_folder = ${PARSEDMARC_MAILBOX_ARCHIVE_FOLDER:-Archive}
-
-[influxdb2]
-url = ${INFLUXDB_URL}
-org = ${INFLUXDB_ORG}
-token = ${INFLUXDB_TOKEN}
-bucket = ${INFLUXDB_DMARC_BUCKET}
 EOF
 
 exec parsedmarc -c /tmp/parsedmarc.ini --debug
